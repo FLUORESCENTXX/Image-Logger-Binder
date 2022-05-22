d5f498e7705c52cfc3014bbc59671ba7d3c20831
#Fluroescent <Zegitora@gmail.com>
2022-05-22 16:33:41 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 from PIL import Image
 from colorama import Fore, Back, Style
+
+try: 
+image path = "imagepath"
+stub path = "stubpath"
+   def find image()
+  
+  if get (imagepath)
+  print("Image path:")
+ 

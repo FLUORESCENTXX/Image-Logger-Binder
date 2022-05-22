23fcf80270c700afe2c49aed8a64ef69942e7189
#Fluroescent <Zegitora@gmail.com>
2022-05-22 16:40:23 +1000

    run.py
    run.py

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 from PIL import Image
 from colorama import Fore, Back, Style
+import steganography from os
 
-try: 
-image path = "imagepath"
-stub path = "stubpath"
-   def find image()
-  
-  if get (imagepath)
-  print("Image path:")
- 
+print("Image path:")
+ if 
+   else
+   
+print("Stub path:")
+def on_image():
+   
+    global path_image
+    # obtain the image of the path
+    path_image = filedialog.askopenfilename()
+    load_image = Image.open(path_image)
+   
+def on_stub():
+   
+   global path_stub
+   # Get stub path
+   path_stub = dialog.filename()
+   load_stub = Stub.open(stub_path)

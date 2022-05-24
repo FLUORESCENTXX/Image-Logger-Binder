814d33775be5261bf10b739c79b9d1a99e5118d7
#Fluroescent <Zegitora@gmail.com>
2022-05-24 16:24:08 +1000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,35 @@
+import requests
+from steganography 
+import colorama
+from image
+
+from Fore, Back, Style
+
+print("fluroxx")
+
+print("credits to him")
+
+input("Image directory")
+input("stub directory")
+
+class image
+ def find image (input)
+ def find openfile
+
+
+class stub
+ def find stub(input)
+ def find openfile
+
+
+try:
+print("Binding...")
+ else
+print("Oops there was an error please fix your stub or image")
+
+print("Binded")
+
+ else 
+
+print("Couldnt bind")
+
